91c78b4d9819f2cff94ed5a538a5ee5263a4f3b7
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2021-06-25 20:41:55 +0200

    updates

--- .p10k.zsh
+++ .p10k.zsh
@@ -55,7 +55,7 @@
     #asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
     #virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     #anaconda                # conda environment (https://conda.io/)
-    pyenv                   # python environment (https://github.com/pyenv/pyenv)
+    #pyenv                   # python environment (https://github.com/pyenv/pyenv)
     #goenv                   # go environment (https://github.com/syndbg/goenv)
     #nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
     nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
@@ -93,17 +93,17 @@
     #vi_mode                 # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
     load                  # CPU load
-    my_cpu_temp
+    #my_cpu_temp
     disk_usage            # disk usage
     ram                   # free RAM
     swap                  # used swap
-    todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
+    #todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     time                    # current time
     # =========================[ Line #2 ]=========================
     newline                 # \n
-    ip                    # ip address and bandwidth usage for a specified network interface
+    #ip                    # ip address and bandwidth usage for a specified network interface
     public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
     # battery               # internal battery
