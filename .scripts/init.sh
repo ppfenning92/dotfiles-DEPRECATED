bc056cc69b0900d04e23fac304fa85dc3395da1e
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2022-02-23 15:44:32 +0100

    fixing init and docs

--- .scripts/init.sh
+++ .scripts/init.sh
@@ -62,7 +62,7 @@ echo "cleanup done"
 
 echo 'Installing ZSH'
 sudo apt install zsh
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh) $(whoami)
 git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/zsh/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.config/zsh/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.config/zsh/plugins/zsh-history-substring-search
